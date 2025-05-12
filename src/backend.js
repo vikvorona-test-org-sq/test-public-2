82e9a6a62a0c3fe67263c74e67c8ef673983d4e7
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-05-12 15:13:07 +0200

    TEST

--- src/backend.js
+++ src/backend.js
@@ -12,6 +12,9 @@ crypto.createCipheriv("DES", key, iv); // Noncompliant
 crypto.createCipheriv("DES", key, iv); // Noncompliant
 crypto.createCipheriv("DES", key, iv); // Noncompliant
 crypto.createCipheriv("DES", key, iv); // Noncompliant
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+crypto.createCipheriv("DES", key, iv); // Noncompliant
 
 location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
